19c921ae5cb1b068e02eca63032648d91c9d1696
Paweł Świtalski <paswitalski@gmail.com>
2024-08-24 17:34:52 +0200

    cart implementing model, add SessionExtensions

---
+++ Restaurant.WebApp/Models/CartLine.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.Business.Models;
+
+namespace Restaurant.WebApp.Models
+{
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+
+        public ProductModel Product { get; set; } = new();
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ Restaurant.WebApp/Models/SessionCart.cs
@@ -0,0 +1,38 @@
+﻿using Restaurant.Business.Models;
+using Restaurant.WebApp.Infrastructure;
+using System.Text.Json.Serialization;
+
+namespace Restaurant.WebApp.Models
+{
+    public class SessionCart : Cart
+    {
+        [JsonIgnore]
+        public ISession? Session { get; set; }
+
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>().HttpContext?.Session;
+            SessionCart cart = session?.GetJson<SessionCart>("Cart") ?? new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+
+        public override void AddItem(ProductModel product, int quantity)
+        {
+            base.AddItem(product, quantity);
+            this.Session?.SetJson("Cart", this);
+        }
+
+        public override void RemoveLine(ProductModel product)
+        {
+            base.RemoveLine(product);
+            this.Session?.SetJson("Cart", this);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            this.Session?.Remove("Cart");
+        }
+    }
+}
